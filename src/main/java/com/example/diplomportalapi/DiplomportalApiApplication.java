8f2ae0d111b55aab769a4d2cafc46f7d29074110
Talha-Butt786 <s195475@student.dtu.dk>
2021-10-15 01:05:11 +0200

    Initial commit

---
+++ src/main/java/com/example/diplomportalapi/DiplomportalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplomportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomportalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomportalApiApplication.class, args);
+    }
+
+}
